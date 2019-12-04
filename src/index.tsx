bfb13f2f21c8e25fbd0629be9dd90e9e69a99a5d
x1aodingdang <1761689749@qq.com>
2019-12-04 17:42:10 +0800

    feat: 结构优化

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/style/reset.scss";
-import App from "./App";
+import App from "./router/index";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
