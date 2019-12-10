93c3d244a12decc59178211e9b2f4e662217b9d0
x1aodingdang <1761689749@qq.com>
2019-12-10 10:56:20 +0800

    feat: 引入了字体图标 & 首页header

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,23 @@
+// icon 字体图标  采用的是阿里的字体图标库   Symbol 方式 生成一个  需要在模板html引入一个js文件
+
+import * as React from "react";
+
+export interface IconProps {
+  className: string;
+}
+
+/**
+ * @description 使用方法 <Icon className='icon-maikefeng'> </Icon>
+ * @description 弃用  使用方法 <Icon>&#xe617;</Icon>
+ */
+export default class Icon extends React.Component<IconProps> {
+  render() {
+    const { className } = this.props;
+    // return <i className="iconfont">{children}</i>;
+    return (
+      <svg className="icon" aria-hidden="true">
+        <use href={`#${className}`}></use>
+      </svg>
+    );
+  }
+}
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,30 +1,19 @@
 // src/components/Hello.tsx
 
 import * as React from "react";
+import Header from "../../components/Header/index";
 
 export interface Props {
   name: string;
   enthusiasmLevel?: number;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: Props) {
-  if (enthusiasmLevel <= 0) {
-    throw new Error("You could be a little more enthusiastic. :D");
-  }
-
-  return (
-    <div className="hello">
-      <div className="greeting">
-        Hello {name + getExclamationMarks(enthusiasmLevel)}
+export default class Home extends React.Component<Props> {
+  render() {
+    return (
+      <div className="home">
+        <Header></Header>
       </div>
-    </div>
-  );
-}
-
-export default Hello;
-
-// helpers
-
-function getExclamationMarks(numChars: number) {
-  return Array(numChars + 1).join("!");
+    );
+  }
 }
