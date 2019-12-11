567840d53d539eba53da11d64aab390f497593be
tutu <tutu@tg10010.com>
2019-12-11 18:59:48 +0800

    feat: 底部tabber

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,33 +3,43 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
+  useLocation
 } from "react-router-dom";
 import routes from "./router";
+import "./app.scss";
+import FooterTabBar from "../components/tabBar";
 
 export default function APP() {
   return (
-    <Router>
-      <Switch>
-        {routes.map((route, i) => (
-          <RouteWithSubRoutes key={i} {...route}></RouteWithSubRoutes>
-        ))}
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          {routes.map((route, i) => (
+            <RouteWithSubRoutes key={i} {...route}></RouteWithSubRoutes>
+          ))}
+        </Switch>
+      </Router>
+      <FooterTabBar />
+    </>
   );
 }
 
 function RouteWithSubRoutes(route: any) {
+  console.log(useLocation(), "useLocation");
+
   return (
-    <Route
-      path={route.path}
-      render={props =>
-        route.redirect ? (
-          <Redirect exact to={{ pathname: route.redirect }}></Redirect>
-        ) : (
-          <route.component {...props} routes={route.routes} />
-        )
-      }
-    ></Route>
+    <div className="body">
+      <Route
+        path={route.path}
+        render={props =>
+          route.redirect ? (
+            <Redirect exact to={{ pathname: route.redirect }}></Redirect>
+          ) : (
+            <route.component {...props} routes={route.routes} />
+          )
+        }
+      ></Route>
+    </div>
   );
 }
