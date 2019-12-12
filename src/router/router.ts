6349ed0651ef3653be48afe49d0673fc6dfaf293
tutu <tutu@tg10010.com>
2019-12-12 15:58:48 +0800

    feat: 底部点击跳转&增加了路由配置

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,22 @@
 import Home from "../views/home/index";
+import Me from "../views/me/index";
+
+// export interface iRouterList {
+
+// }
 
 export default [
   {
     path: "/home",
-    component: Home
+    component: Home,
+    tabBar: true, // 展示 底部 菜单
+    header: true // 展示搜索头
+  },
+  {
+    path: "/me",
+    component: Me,
+    tabBar: true, // 展示 底部 菜单
+    header: false // 展示搜索头
   },
   // {
   //   path: "/tacos",
---
+++ src/views/me/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default class Me extends React.Component {
+  render() {
+    return <div>我的我的我的哦</div>;
+  }
+}
