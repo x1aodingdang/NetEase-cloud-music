755d6817373c1ada284004f4adaf5d0dfd306a42
x1aodingdang <1761689749@qq.com>
2019-12-18 14:41:52 +0800

    faet: 内容开发中页面

--- src/views/account/index.tsx
+++ src/views/account/index.tsx
@@ -5,10 +5,10 @@ import NoOpen from "../../components/NoOpen";
 // 创建类型接口
 export interface IProps {}
 
-class Me extends React.Component<IProps> {
+class Account extends React.Component<IProps> {
   render() {
     return <NoOpen></NoOpen>;
   }
 }
 
-export default connect()(Me);
+export default connect()(Account);
